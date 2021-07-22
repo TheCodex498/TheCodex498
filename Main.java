ded648b10a06037a1812b0727ee8b646a2ba9418
TheCodex498 <77007646+TheCodex498@users.noreply.github.com>
2021-07-22 20:48:21 +0530

    AreaCalculator

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double areaOfRectangle = AreaCalculator.area(4.00,5.00);
+        double areaOfCircle = AreaCalculator.area(5.00);
+
+        System.out.println(areaOfRectangle);
+        System.out.println(areaOfCircle);
+
+    }
+}
